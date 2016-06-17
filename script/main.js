367b1756b70cd1787943f841fddaf2c53ccb4580
Ilya Novozhilov <Ilya_Novozhilov@epam.com>
2016-06-17 20:02:12 +0300

    add css style

--- script/main.js
+++ script/main.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
     var buttons = [];
 
-    buttons.push(new Button("кнопка1", function(){alert("кнопка1");}, ".button"));
-    buttons.push(new Button("кнопка2", function(){alert("кнопка2");}, ".button1"));
+    buttons.push(new Button("кнопка1", function(){alert("кнопка1");}, "button"));
+    buttons.push(new Button("кнопка2", function(){alert("кнопка2");}, "button1"));
 
 
-    var newModalWindow = new ModalWindow("Окно", "моё модальное окно", buttons, ".new-modal-window", new Overlay(".overlay"));
+    var newModalWindow = new ModalWindow("Окно", "моё модальное окно", buttons, "modal_form", new Overlay("overlay"));
     
     $("body").append($(newModalWindow.window));
+    
+    newModalWindow.show();
 });
